37b4845d42596912f9eba5f196c8740c7abb78a9
JairoHernandez <jairomh@hotmail.com>
2017-04-05 12:09:35 -0500

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,39 @@
+// Object destructure.
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+/**In production environment this will connect to an AWS or Heroku url.*/
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        // return is necessary to stop the program or alternatively remove return and add 'else' statement.
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server.');
+
+    // Google the 2nd parameter 'mongodb update operator' for findOneAndUpdate,
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('58e4131b2c6dae5b43be0ee7')
+    // }, {
+    //     $set: {completed: true, name: "Jairo"}
+    // }, {
+    //     returnOriginal: false // returns updated data.
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('58e51a507402e440ce61199a')
+    }, {
+        $set: {name: "Jairo"},
+        $inc: {age: 1}
+    }, {
+        returnOriginal: false // returns updated data.
+    }).then((result) => {
+        console.log(result);
+    });
+
+    db.close();
+}); 
